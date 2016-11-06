ace95fad380ddf4fce7d741b3001223556609668
preethibaskar <preethibaskar90@gmail.com>
2016-11-05 18:56:26 -0700

    Change to render stmt+ adding mongo

--- ReferApp/settings.py
+++ ReferApp/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'ReferApp.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'referApp_db'
+   }
 }
 
 
--- howdy/views.py
+++ howdy/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        return render(request, 'index.html')
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
